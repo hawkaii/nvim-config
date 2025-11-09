e579715727a99b8fd430d76ab49b5004af54c131
Parthib Mukherjee <parthibmukherjee@gmail.com>
2025-11-09 03:29:58 +0000

    feat(plugins): add vim-fugitive git integration
    Signed-off-by: Parthib Mukherjee <parthibmukherjee@gmail.com>

---
+++ lua/plugins/fugitive.lua
@@ -0,0 +1,8 @@
+return {
+  -- Git integration via fugitive
+  {
+    "tpope/vim-fugitive",
+    cmd = { "Git", "G", "Gdiffsplit", "Gread", "Gwrite", "Gbrowse" },
+    -- You can add keymaps or config here if desired
+  },
+}
